7471efe327e4f628b3ac2de50fb6a291f0bbad0d
Chandupa Ranawaka <116500003+Chandupa2001@users.noreply.github.com>
2025-07-08 23:44:42 +0530

    Created password validator middleware and developed change password function

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,25 +2,26 @@ import jwt from 'jsonwebtoken';
 import bycrypt from 'bcrypt';
 import validator from 'validator';
 import userModel from '../models/userModel.js';
+import { passwordValidator } from '../middleware/passwordValidator.js';
 
 // login user
-const loginUser = async (req,res) => {
+const loginUser = async (req, res) => {
     const { username, password } = req.body;
     try {
         const user = await userModel.findOne({ username });
 
         if (!user.status) {
-            res.json({success: false, message: "User is deactivated"});
+            res.json({ success: false, message: "User is deactivated" });
             return;
         }
 
         if (!user) {
-            res.json({success: false, message: "Invalid username or password"});
+            res.json({ success: false, message: "Invalid username or password" });
         }
-        
+
         const isMatch = await bycrypt.compare(password, user.password);
         if (!isMatch) {
-            return res.json({success: false, message: "Invalid username or password"})
+            return res.json({ success: false, message: "Invalid username or password" })
         }
 
         const token = createToken(user._id);
@@ -29,40 +30,41 @@ const loginUser = async (req,res) => {
         user.lastLogin = localTime;
         await user.save();
 
-        res.json({success: true, token: token});
+        res.json({ success: true, token: token });
 
     } catch (error) {
         console.error(error);
-        res.json({success: false, message: "Error"});
+        res.json({ success: false, message: "Error" });
     }
 }
 
 const createToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET)
+    return jwt.sign({ id }, process.env.JWT_SECRET)
 }
 
 // register user
-const registerUser = async (req,res) => {
-    const {firstname, lastname, username, email, role, password} = req.body;
+const registerUser = async (req, res) => {
+    const { firstname, lastname, username, email, role, password } = req.body;
     try {
         // checking is user already exists
-        const exists = await userModel.findOne({email});
+        const exists = await userModel.findOne({ email });
         if (exists) {
-            return res.json({success:false, message: "User already exists"})
+            return res.json({ success: false, message: "User already exists" })
         }
 
         //validating email format and strong password
         if (!validator.isEmail(email)) {
-            return res.json({success:false, message: "Please enter a valid email address"})
+            return res.json({ success: false, message: "Please enter a valid email address" })
         }
 
-        if (password.length < 8 ) {
-            return res.json({success: false, message: "Please enter a strong password"})
+        const validationError = passwordValidator(password);
+        if (validationError) {
+            return res.json({ success: false, message: validationError });
         }
 
         // hashing user password
         const salt = await bycrypt.genSalt(10);
-        const hashedPassword = await bycrypt.hash(password,salt);
+        const hashedPassword = await bycrypt.hash(password, salt);
 
         const newUser = new userModel({
             firstname: firstname,
@@ -75,11 +77,11 @@ const registerUser = async (req,res) => {
 
         const user = await newUser.save();
         const token = createToken(user._id)
-        res.json({success: true, token});
+        res.json({ success: true, token });
 
     } catch (error) {
         console.error(error)
-        res.json({success: false, message: "Error"})
+        res.json({ success: false, message: "Error" })
     }
 }
 
@@ -90,35 +92,35 @@ const updateUserStatus = async (req, res) => {
         const user = await userModel.findOne({ username });
 
         if (!user) {
-            res.json({success: false, message: "Invalid username"});
+            res.json({ success: false, message: "Invalid username" });
         }
 
-        await userModel.findOneAndUpdate({ username }, {status: status} );
+        await userModel.findOneAndUpdate({ username }, { status: status });
 
-        res.json({success: true, message: "User status updated successfully"});
+        res.json({ success: true, message: "User status updated successfully" });
 
     } catch (error) {
         console.log("Error updating user status: ", error);
-        res.json({success: false, message: "Error updating user status"});
+        res.json({ success: false, message: "Error updating user status" });
     }
 }
 
 // edit user details
-const editUser = async (req,res) => {
+const editUser = async (req, res) => {
     try {
         const { username } = req.body;
 
         const user = await userModel.findOne({ username });
 
         if (!user) {
-            res.json({success: false, message: "Invalid username"});
+            res.json({ success: false, message: "Invalid username" });
         }
 
-        await userModel.findOneAndUpdate({username: username}, {firstname: req.body.name, lastname: req.body.lastname, email: req.body.email, role: req.body.role})
-        res.json({ success: true, message: "User updated"})
+        await userModel.findOneAndUpdate({ username: username }, { firstname: req.body.name, lastname: req.body.lastname, email: req.body.email, role: req.body.role })
+        res.json({ success: true, message: "User updated" })
     } catch (error) {
         console.error(error);
-        res.json({ success: false, message: "Error"});
+        res.json({ success: false, message: "Error" });
     }
 }
 
@@ -143,5 +145,43 @@ const deleteUser = async (req, res) => {
     }
 }
 
+const changePassword = async (req, res) => {
+    const { username, oldpassword, newpassword, newpassword2 } = req.body;
 
-export { loginUser, registerUser, updateUserStatus, editUser, deleteUser };
+    const user = await userModel.findOne({ username });
+
+    try {
+        if (!user) {
+            return res.json({ success: false, message: "Invalid username" });
+        }
+
+        const isMatch = await bycrypt.compare(oldpassword, user.password);
+        if (!isMatch) {
+            return res.json({ success: false, message: "Invalid password" });
+        }
+
+        if (newpassword !== newpassword2) {
+            return res.json({ success: false, message: "New passwords do not match" });
+        }
+
+        const validationError = passwordValidator(newpassword);
+        if (validationError) {
+            return res.json({ success: false, message: validationError });
+        }
+
+        // Update the password
+        const salt = await bycrypt.genSalt(10);
+        const hashedPassword = await bycrypt.hash(newpassword, salt);
+        user.password = hashedPassword;
+        await user.save();
+
+        res.json({ success: true, message: "Password changed successfully" });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Something went wrong" });
+    }
+}
+
+
+export { loginUser, registerUser, updateUserStatus, editUser, deleteUser, changePassword };
