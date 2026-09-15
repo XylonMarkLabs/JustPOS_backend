cd0ee13484f3910c16b94dad23443e65ef0fd484
Chandupa Ranawaka <116500003+Chandupa2001@users.noreply.github.com>
2026-09-15 18:48:23 +0530

    Prepare for deployment v1

--- server.js
+++ server.js
@@ -21,7 +21,12 @@ const port = 4000
 
 // middleware
 app.use(express.json())
-app.use(cors())
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true
+    })
+);
 
 // db connection
 connectDB();
